62e43f8d42a796b4c4533b4137f7c0e2d18adacc
Dailin Romero <dailinromero123@gmail.com>
2024-05-22 13:57:38 -0500

    barra navegacion

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+
+    <header className="header p-3 mb-3 border-bottom">
+
+      <div className="logo">
+        <a href="">
+           <p>Logo</p>
+        </a>
+      </div>
+      
+      <div className="barra ">
+       <p>Panel del psicologo</p>
+      </div>
+
+      
+       <div className="dropdown text-end perfil">
+        <a href="#" className="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          <strong>Fulanita Perez </strong>
+          &nbsp; &nbsp;
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
+        </a>
+        
+        <ul className="dropdown-menu text-small">
+            <li><a className="dropdown-item" href="#">New project...</a></li>
+            <li><a className="dropdown-item" href="#">Settings</a></li>
+            <li><a className="dropdown-item" href="#">Profile</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">Sign out</a></li>
+        </ul>
+        
+        <p >Psicologo</p>
+      </div> 
+
+    </header>
+  )
+}
