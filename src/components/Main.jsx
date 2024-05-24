5eafec178cbf5ac0cbeb77464f7653bc30332cf5
Dailin Romero <dailinromero123@gmail.com>
2024-05-24 13:44:49 -0500

    mensaje

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import '../main.css';
 
 export const Main = () => {
   return (
+    
     <div className="main">
-      <p>hoa anjdjdh </p>
+      
+      <form className="d-flex search-container">
+          <input className="form-control search me-2" type="text" placeholder="Search for something" />
+      </form>
+
+      <div className='boton-agendar'>
+        <button type="button" class="btn btn-primary">Agendar sesión</button>
+      </div>
+      
+      <div className="sesiones-cont">
+
+      </div>
     </div>
   )
 }
