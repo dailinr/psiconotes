4fbde441a2302404de98ffdec2aee6f3b6bcce6d
Dailin Romero <dailinromero123@gmail.com>
2024-05-23 23:24:41 -0500

    add icons

---
+++ src/fontawesome.js
@@ -0,0 +1,5 @@
+// src/fontawesome.js
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee, faUser } from '@fortawesome/free-solid-svg-icons'; // Importa los iconos que necesitas
+
+library.add(faCoffee, faUser); // Añade los iconos a la librería
