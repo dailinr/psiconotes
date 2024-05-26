f4f3574f909cde8a433cedf96f85107f500e137d
valenboc <144183321+valenboc@users.noreply.github.com>
2024-05-25 21:25:43 -0500

    Calendario pte 1

--- src/components/Calendario.jsx
+++ src/components/Calendario.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Calendar, dayjsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import dayjs from 'dayjs';
+import 'dayjs/locale/es'; // Importar la localización de dayjs en español
+
+// Configurar dayjs para usar la localización en español
+dayjs.locale('es');
 
 export const Calendario = () => {
+  const localizer = dayjsLocalizer(dayjs);
+
+  const events = [
+    {
+      start: dayjs('2024-05-25T12:00:00').toDate(),
+      end: dayjs('2024-05-25T13:00:00').toDate(),
+      title: "Cita con Pepe"
+    }
+  ];
+
   return (
-    <div>
-      <h2>Calendario</h2>
-      <p>Aquí va el contenido del calendario.</p>
+    <div className="card">
+      <div className="card-body">
+        <div className="sesiones-cont">
+          <p>Calendario</p>
+        </div>
+        <div style={{ height: '45vh', width: '100%' }}>
+          <Calendar
+            localizer={localizer}
+            events={events}
+            views={["month", "day", "agenda"]}
+            min={dayjs('2024-05-25T08:00:00').toDate()}
+            max={dayjs('2024-05-25T19:00:00').toDate()}
+            messages={{ next: "Siguiente", previous: "Anterior", today: "Hoy", month: "Mes", day: "Día", date: "Fecha", time: "Hora", event: "Evento" }}
+            formats={{
+              dayHeaderFormat: date =>{
+                return dayjs(date).format("dddd DD/MM/YYYY")
+              }
+            }}
+            className="custom-calendar"/>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Sesiones.jsx
+++ src/components/Sesiones.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { FilaSesion } from './FilaSesion.jsx'
 
 export const Sesiones = () => {
   return (
     <div>
-      <h2>Sesiones</h2>
-      <p>Aquí va el contenido de las sesiones</p>
+    <div className="search-and-button-container">
+      <form className="d-flex search-container">
+          <input className="form-control search me-2 shadow-sm" type="text" placeholder="Search for something" />
+      </form>
+
+      <div className='boton-agendar'>
+        <button type="button" class="btn btn-primary shadow">
+          <i class="bi bi-person-add"></i>
+          &nbsp; Agendar sesión</button>
+      </div>
+      </div>
+      
+      <div className="sesiones-cont">
+        <div>
+          <p >Sesiones 
+            <span className="filter">
+              Filtrar <i class="bi bi-caret-down-fill"></i>
+            </span>
+          </p> 
+        </div>
+
+        <FilaSesion/>
+        <FilaSesion/>
+        <FilaSesion/>
+        <FilaSesion/>
+        
+      </div>
     </div>
   );
 };
