2ddf526a36ffd33a4b5c326f1128cf99b00d0ee5
JesuhT <eliseotorres1798@gmail.com>
2024-06-04 05:47:36 -0500

    notifications

--- src/components/Notificaciones.jsx
+++ src/components/Notificaciones.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState } from "react";
+import "../css/notifications.css";
 
-export const Notificaciones = () => {
+function Notificaciones() {
+  const [info, setInfo] = useState([
+    {
+      id: "1",
+      estudiante: "Valentina",
+      psicologo: "Milicen",
+      titulo: "Sesión cancelada",
+      motivo: "Si me muevo me cago, lorem ipsum traquen truqui pum pum",
+      unread: true
+    }
+  ]);
+
+  const markAsRead = () => {
+    const updatedInfo = info.map((notification) => ({
+      ...notification,
+      unread: false
+    }));
+    setInfo(updatedInfo);
+  };
+
+  const countNoti = info.filter(notification => notification.unread).length;
+
   return (
-    <div>
-      <h2>Notificaciones</h2>
-      <p>Aquí va el contenido de las Notificaciones</p>
-    </div>
+    <section className="section-notification">
+      <div className="navi">
+        <h3 className="noti-title">
+          Notifications <span className="noti-count">{countNoti}</span>
+        </h3>
+        <a className="noti-markasread" onClick={markAsRead}>Mark all as read</a>
+      </div>
+
+      <div className="noti-content">
+        {info.map((notification) => (
+          <div key={notification.id} className={`noti noti-message ${notification.unread ? 'noti-unread' : ''}`}>
+            <img
+              src="../public/icon_student.png"
+              alt="mark webber image"
+              className="avatar"
+            />
+            <div className="noti-text">
+              <p>
+                <span className="link">{notification.psicologo}</span> canceló la sesión
+                <span className={`red-dot ${notification.unread ? 'dot1' : ''}`}></span>
+              </p>
+              <p className="noti-message-message">{notification.motivo}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
-};
+}
 
 export default Notificaciones;
